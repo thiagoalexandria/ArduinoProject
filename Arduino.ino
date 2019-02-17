a2e7029b6cf187b366b9aeb60a1f6dd310438859
Thiago Alexandria <thiagoo_oa@hotmail.com>
2019-02-16 22:27:03 -0300

    documentação e ajustes no menu, inclusão da leitura do ldr

--- Arduino.ino
+++ Arduino.ino
@@ -1,39 +1,49 @@
 
 /******************************************
  * Autor: Thiago Alexandria
- * Arduino TCC Project / Mais informações em breve
- * João Pessoa - PB / 10/02/2019
+ * Arduino TCC Project / 
+ * João Pessoa - PB / 16/02/2019
  * 
  ******************************************/
-
-
-#include <SPI.h>
 #include <Ethernet.h>
-#define AGENTPING 1 //Disponivel
-#define VERSION 1.0 //Versao
-#define LDR 180 //Valor fixo LDR para teste
-#define USO 20  //Valor fixo USO para teste
+#include <SPI.h>
 
+//Declaração para interface Ethernet e porta de comunicação
+#define MACADDRESS 0xA4, 0x28, 0x72, 0xCA, 0x55, 0x2F 
+#define IP 192,168,0,3
+#define LISTENPORT 10050
 
-//Informacoes de endereco IP, gateway, mascara de rede
-byte mac[] = { 0xA4, 0x28, 0x72, 0xCA, 0x55, 0x2F };    //MAC
-byte ip[] = { 192, 168, 0, 3 };                         //IP
-byte gateway[] = { 192, 168, 0, 1 };                    //IP GATEWAY
-byte subnet[] = { 255, 255, 255, 0 };                   //IP MASK
+//VARIÁVEIS GLOBAIS
+#define USO 15                // Valor fixo para uso em %
+#define ZABBIXPING 1          //Resposta Ping 
+#define ZABBIXPROTOCOL 3.4    //Versão do protocolo 3.4
+#define ITEMS_SIZE  7         //TAMANHO MÁXIMO DA LSITA DE ITENS
 
-EthernetServer server(10050);   //ZABBIX CLIENT PORT
-EthernetClient client;          //START CLIENT
-String hostname = "monitor.ino";
+//VARIÁVEIS NÃO GLOBAIS
+//COMANDOS ACEITOS
+String items[] = { "agent.ping", "agent.hostname", "agent.version", "agent.ldr", "agent.uso" };
+//HOSTNAME DO DISPOSITIVO
+String hostname = "tcc-monitor";
+//SENSORES
+int LDR1 = A0; int LDR2 = A1;
+//OUTRAS VARIÁVEIS
+int sensor1 = 0; int sensor2 = 0; int MEDIA = 0; String msg ="";
 
-String opcao[] = {"monitor.ping", "monitor.hostname", "monitor.version", "monior.ldr", "monitor.uso"};
+//INICIANDO O SERVER NA PORTA 10050
+EthernetServer server = EthernetServer(LISTENPORT);
 
-
+//SETUP, INICIANDO O DISPOSITIVO
 void setup() {
-  Ethernet.begin(mac, ip, gateway, subnet);
+  //NETWORK
+  uint8_t mac[6] = {MACADDRESS};
+  uint8_t ip[4] = {IP};
+  delay(1000);
+  Ethernet.begin(mac,ip);
   server.begin();
 }
 
-
+//LOOP COM O SWITCH CASE RESPONSÁVEL POR RECEBER
+//O COMANDO E INFORMAR O QUE DEVE SER RETORNADO
 void loop() {
   EthernetClient client = server.available();
   if (client) {
@@ -43,16 +53,16 @@ void loop() {
 
         switch(findId(msg)){
           case 0:
-            server.println(AGENTPING);
+            server.println(ZABBIXPING);
             break;
           case 1:
             server.println(hostname);
             break;
           case 2:
-            server.println(VERSION);
+            server.println(ZABBIXPROTOCOL);
             break;
           case 3:
-            server.println(LDR);
+            server.println(ldrLer(sensor1,sensor2));
             break;
           case 4:
             server.println(USO);
@@ -70,12 +80,23 @@ void loop() {
   }
 }
 
+//FUNÇÃO PARA TRATAR O COMANDO RECEBIDO E BUSCAR
+//NA LISTA DE COMANDOS A SUA POSIÇÃO NA LISTA
 int findId(String text) {
   int returnValue=-1;
-  for (int i=0; i < OPCAO_SIZE; i++){
-    if(opcao[i].equals(text)){
+  for (int i=0; i < ITEMS_SIZE; i++){
+    if(items[i].equals(text)){
       returnValue = i; 
     }
   }
   return returnValue;
 }
+
+//FUNÇÃO PARA LEITURA E RETORNO DA MÉDIA DOS LDRS
+int ldrLer(int sensor1, int sensor2){
+   sensor1 = analogRead(LDR1);
+   sensor2 = analogRead(LDR2);
+   MEDIA=(sensor1+sensor2)/2;
+   return MEDIA;
+}
+
