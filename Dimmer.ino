e2feccb8bb630f075396a854c5dabd683efa68c5
Thiago Alexandria <thiagoo_oa@hotmail.com>
2019-03-23 22:06:41 -0300

    Configuração de placa como slave para realização de processo de forma independente, encaminhando os valores de utilização através de conexão serial para placa Master

--- Dimmer.ino
+++ Dimmer.ino
@@ -3,17 +3,23 @@
 * Arduino TCC Project / Mais informações em breve
 * João Pessoa - PB / 03/03/2019
 * 
-* Esta função irá ler os valores coletados no LDR e tratará esta função servirá de aṕoio para 
-* o controle de energia de fluxo teremos divisões através de níveis de sensibilidade de lux.
-* valores referentes aos status high, established, low poderá sofrer alteração de acordo com a
-* a NBR 5413 de iluminação de interiores.
+* Esta função será aplicada a placa Slave do projeto, essa placa será responsável 
+* por realizar a leitura constante dos valores obtidos através dos LDRS. Baseado 
+* na média de Lux obtidos a placa encaminhará os valores para o módulo dimmer rea- 
+* lizando a dimerização das lampadas de  acordo com a NBR 5413 de iluminação de 
+* interiores, encaminhando os dados de energia para a placa Master através de uma
+* comunicação serial. 
+
 ******************************************/
 
 #define PINO_ZC 2
 #define PINO_DIM 9
-int LDR1 = A0;
-int sensor1 = 0;
+int LDR1 = A0; int LDR2 = A1; int LDR3 = A2; int LDR4 = A3;
 
+//OUTRAS VARIÁVEIS
+int state_ldr1 = 0; int state_ldr2 = 0;
+int state_ldr3 = 0; int state_ldr4 = 0;
+int state_media = 0;
 volatile long luminosidade = 0;  // 0 a 100 
  
 void zeroCross()  {
@@ -29,27 +35,38 @@ void zeroCross()  {
 void setup() {
   pinMode(PINO_DIM, OUTPUT);
   attachInterrupt(0, zeroCross, RISING);
+  Serial.begin(9600);
 }
  
 void loop() {
-  sensor1 = analogRead(LDR1);
-  
-  if (sensor1 > 500){
+
+  state_ldr1 = analogRead(LDR1);
+  state_ldr2 = analogRead(LDR2);
+  state_ldr3 = analogRead(LDR3);
+  state_ldr4 = analogRead(LDR4);
+
+  state_media=(state_ldr1+state_ldr2+state_ldr3+state_ldr4)/4;
+
+ if (state_media > 500){
     for (byte i=10; i<50; i++) {
       luminosidade=25;
-     delay(15);     
+      Serial.write(25);
+      delay(15);     
     }
   }
-  if (sensor1 < 500 && sensor1 > 300){
+  
+  if (state_media < 500 && state_media > 300){
     for (byte i=10; i<50; i++) {
       luminosidade=50;
+      Serial.write(50);
      delay(15);     
     }
   }
 
-  if (sensor1 < 300){
+  if (state_media < 300){
     for (byte i=10; i<50; i++) {
       luminosidade=90;
+      Serial.write(90);
      delay(15);     
     }
   }
